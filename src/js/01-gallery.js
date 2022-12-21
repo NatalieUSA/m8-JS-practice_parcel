91b5320baf1af2600622aeed0c98a51723896d2a
NatalieUSA <110269066+NatalieUSA@users.noreply.github.com>
2022-12-21 07:28:25 +0200

    add library

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,7 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
 // Change code below this line
 
 console.log(galleryItems);
+console.log(SimpleLightbox);
